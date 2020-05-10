a82201cae11f333fdb9fb4e9e38873134d20b759
hanlongfu <hfu@salemstate.edu>
2020-05-10 15:57:38 -0700

    initial setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,36 @@
-// Global app controller
-import num from './test'
+import Search from "./models/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
+//global state of the app (a.k.a. central warehouse)
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - liked recipes
+const state = {};
 
-console.log(`I imported ${num} from another module.`);
+// action when user click on "search"
+const controlSearch = async () => {
+	// 1) get query from view
+	const query = searchView.getInput();
 
+	if (query) {
+		// 2) New search object and add to state
+		// new instance based on Search class
+		state.search = new Search(query);
+
+		// 3) Prepare UI for results
+
+		// 4) Search for recipes
+		await state.search.getResults();
+
+		// 5) render results on UI
+		searchView.renderResults(state.search.result);
+	}
+};
+
+//When users click on "search"
+elements.searchForm.addEventListener("submit", (e) => {
+	// stops default reloading when search is clicked
+	e.preventDefault();
+	controlSearch();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 //path package in node to allow absolute path
 const path = require("path");
+//copy index.html into './dist'
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -9,18 +10,22 @@ module.exports = {
 		filename: "js/bundle.js",
 	},
 	devServer: {
+		//folder to display on server
 		contentBase: "./dist",
 	},
 	plugins: [
+		//constructor function for HtmlWebpackPlugin
 		new HtmlWebpackPlugin({
 			filename: "index.html",
 			template: "./src/index.html",
 		}),
-  ],
-  //babel loader config
+	],
+	//babel loader config
 	module: {
 		rules: [
+			//each loader needs an config object
 			{
+				//test to see if files end with .js
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
