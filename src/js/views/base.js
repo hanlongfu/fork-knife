b2b7e172b9146b37fdf5c5a1ae8f6ec88c575ad0
hanlongfu <hfu@salemstate.edu>
2020-05-10 23:15:52 -0700

    pagination finish

---
+++ src/js/views/base.js
@@ -0,0 +1,34 @@
+//an object that contains all the selections of DOM elements
+//this is similar to DOMstrings in Budgety app
+export const elements = {
+	searchForm: document.querySelector(".search"),
+	searchInput: document.querySelector(".search__field"),
+	searchResList: document.querySelector(".results__list"),
+	searchRes: document.querySelector(".results"),
+	searchResPages: document.querySelector(".results__pages"),
+};
+
+export const elementStrings = {
+	loader: "loader",
+};
+
+// render loader that can be reused for loading results (in 2 different places)
+export const renderLoader = (parent) => {
+	const loader = `
+		<div class="${elementStrings.loader}">
+			<svg>
+				<use href="img/icons.svg#icon-cw"></use>
+			</svg>
+		</div>
+	`;
+	parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+//clear loader
+export const clearLoader = () => {
+	const loader = document.querySelector(`.${elementStrings.loader}`);
+	if (loader) {
+		// delete loader
+		loader.parentElement.removeChild(loader);
+	}
+};
---
+++ src/js/views/searchView.js
@@ -0,0 +1,117 @@
+import { elements } from "./base";
+
+//obtain input value of the search field
+export const getInput = () => elements.searchInput.value;
+
+//clear input field before and after search
+export const clearInput = () => {
+	elements.searchInput.value = "";
+};
+
+//clear results (including pagination button) to make space for new results
+export const clearResults = () => {
+	elements.searchResList.innerHTML = "";
+	//pagination buttons
+	elements.searchResPages.innerHTML = "";
+};
+
+// limit the reciple title to 17 characters
+const limitRecipeTitle = (title, limit = 17) => {
+	//create an empty array to hold the new title
+	const newTitle = [];
+	if (title.length > limit) {
+		//title split into an array of words
+		title.split(" ").reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
+
+		//return the result
+		return `${newTitle.join(" ")}...`;
+	}
+	return title;
+};
+
+//render individual recipe result on UI
+//each recipe object has the following properties
+// f2f_url, image_url, publisher, publisher_url, recipe_id, social rank, title, source_url
+const renderRecipe = (recipe) => {
+	const markup = `
+    <li>
+        <a class="results__link" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="${limitRecipeTitle(
+		recipe.title
+	)}">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+  `;
+	//insert markup under ul with class name ".results__list"
+	elements.searchResList.insertAdjacentHTML("beforeend", markup);
+};
+
+/*
+  create pagination button
+*/
+
+//type: 'prev' or 'next'
+// ${type === 'prev' ? page - 1: page + 1}  ==> this displays the number on the pagination button
+// 'data-goto' allows us to know which page to go to when clicked
+const createButton = (page, type) => `
+  <button class="btn-inline results__btn--${type}" data-goto = ${
+	type === "prev" ? page - 1 : page + 1
+}>
+		<span>Page ${type === "prev" ? page - 1 : page + 1}</span>			
+		<svg class="search__icon">
+						<use href="img/icons.svg#icon-triangle-${
+							type === "prev" ? "left" : "right"
+						}"></use>
+		</svg>
+      
+  </button>
+`;
+
+// render backward and forward pagination buttons
+const renderButtons = (page, numResults, resPerPage) => {
+	// 4.5 pages -> 5 pages
+	const pages = Math.ceil(numResults / resPerPage);
+
+	let button;
+	// page 1: only display forward button to page 2
+	if (page === 1 && pages > 1) {
+		button = createButton(page, "next");
+		// in-between page: display both backward and forward button
+	} else if (page < pages) {
+		button = `
+      ${createButton(page, "prev")}
+      ${createButton(page, "next")}
+    `;
+		// last page: only display backward button (results- resPerPage)
+	} else if (page === pages && pages > 1) {
+		button = createButton(page, "prev");
+	}
+
+	elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+//render recipe search results including pagination on UI
+//set the default position as page 1, each page has 10 results
+export const renderResults = (recipes, page = 2, resPerPage = 10) => {
+	//render results of current page
+	//set the starting and ending point
+	const start = (page - 1) * resPerPage;
+	const end = page * resPerPage;
+
+	// not including the end
+	recipes.slice(start, end).forEach(renderRecipe);
+
+	//render pagination buttons
+	renderButtons(page, recipes.length, resPerPage);
+};
