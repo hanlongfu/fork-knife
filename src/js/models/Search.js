d018b2846c2f734fd078ade6d5a5b2ffca5536bc
hanlongfu <hfu@salemstate.edu>
2020-05-19 21:15:47 -0700

    last

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,19 +1,20 @@
 import axios from "axios";
 
+//search data model
 export default class Search {
-	constructor(query) {
+	constructor(query){
 		this.query = query;
 	}
-	async getResults() {
-		try {
-			const res = await axios(
-				`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`
-      );
-      //recipes data are stored inside result property
+	async getResults () {
+		try{
+			const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+	
+			// res.data.recipes are data read back from the api
 			this.result = res.data.recipes;
-			//console.log(this.result);
-		} catch (e) {
+		} catch(e) {
 			alert(e);
-		}
+		}		
 	}
 }
+
+
